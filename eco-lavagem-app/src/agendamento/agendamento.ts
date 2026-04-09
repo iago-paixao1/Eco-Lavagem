f14dafb4a33b0ac34ccdc2905e2e32f5adcd0fc7
Victor Freitas <victorfreitas844@gmail.com>
2026-04-08 21:43:32 -0300

    crud de agedamento

--- eco-lavagem-app/src/agendamento/agendamento.ts
+++ eco-lavagem-app/src/agendamento/agendamento.ts
@@ -19,6 +19,39 @@ export class Agendamento {
   ) {}
 
   ngOnInit(): void {
+    this.limparFormulario()
+    this.agendamentosList = this.agendamentoService.getAll()
+  }
+
+  onSubmit(): void {
+    if (this.formAgendamento.valid) {
+      if (this.idSelecionado != 0) {
+        this.agendamentoService.update(this.idSelecionado, this.formAgendamento.value);
+        this.idSelecionado = 0;
+      } else {
+        this.agendamentoService.create(this.formAgendamento.value);
+      }
+
+
+      this.limparFormulario();
+
+      this.agendamentosList = this.agendamentoService.getAll()
+    }
+  }
+
+  selecionarId(id:number):void {
+    this.idSelecionado = id
+    this.carregarFormulario()
+  }
+  
+  onExcluir(): void{
+    console.log('ID',this.idSelecionado)
+    this.agendamentoService.delete(this.idSelecionado)
+    this.agendamentosList = this.agendamentoService.getAll()
+    this.limparFormulario();
+  } 
+
+  limparFormulario(): void {
     this.formAgendamento = this.fb.group({
       nome: ['', Validators.required],
       veiculo: ['', Validators.required],
@@ -27,30 +60,18 @@ export class Agendamento {
       profissional: ['', Validators.required],
       status: ['Pendente', Validators.required],
     });
-
-    this.agendamentosList = this.agendamentoService.getAll()
   }
 
-  onSubmit(): void {
-    if (this.formAgendamento.valid) {
-      this.agendamentoService.create(this.formAgendamento.value);
+  carregarFormulario(): void {
+    const agendamento = this.agendamentoService.getById(this.idSelecionado)
 
-      this.formAgendamento.reset({
-        status: 'Pendente',
-      });
-
-      console.log('Agendamento criado!');
-      this.agendamentosList = this.agendamentoService.getAll()
-    }
+    this.formAgendamento = this.fb.group({
+      nome: [agendamento?.nome, Validators.required],
+      veiculo: [agendamento?.veiculo, Validators.required],
+      servico: [agendamento?.servico, Validators.required],
+      horario: [agendamento?.horario, Validators.required],
+      profissional: [agendamento?.profissional, Validators.required],
+      status: [agendamento?.status, Validators.required],
+    });
   }
-
-  selecionarId(id:number):void {
-    this.idSelecionado = id
-  }
-  
-  onExcluir(): void{
-    console.log('ID',this.idSelecionado)
-    this.agendamentoService.delete(this.idSelecionado)
-    this.agendamentosList = this.agendamentoService.getAll()
-  } 
 }
